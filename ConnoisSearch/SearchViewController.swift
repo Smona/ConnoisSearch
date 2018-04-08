9da48daa89f1422d6af22386e229d31629360e97
Het Bharucha <bharuchahet@gmail.com>
2018-04-08 18:51:44 -0500

    random recipe request function added
    JSON parsing still needs to be done

--- ConnoisSearch/SearchViewController.swift
+++ ConnoisSearch/SearchViewController.swift
@@ -34,6 +34,9 @@ class SearchViewController: UIViewController, UITextFieldDelegate {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func randomButton(_ sender: Any) {
+        apiConnection()
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "searchID" {
             print("YAY")
@@ -42,5 +45,42 @@ class SearchViewController: UIViewController, UITextFieldDelegate {
             print("YAY")
         }
     }
-
+    
+    func apiConnection() {
+        if let url = NSURL(string:"https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=1") {
+            let request = NSMutableURLRequest(url:url as URL)
+            request.httpMethod = "GET"
+            request.addValue("P94NjqGeAkmshBXL0yGbztKyClfLp1JXYgUjsnvPETR0q1LSkb", forHTTPHeaderField: "X-Mashape-Key")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            
+            let task = URLSession.shared.dataTask(with: request as URLRequest) {
+                (data, response, error) in
+                guard error == nil else {
+                    print(error!)
+                    return
+                }
+                guard let data = data else {
+                    print("data is empty")
+                    return
+                }
+                do {
+                    guard let json = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] else {
+                        print("error trying to convert data to JSON")
+                        return
+                    }
+                    print(json.description)
+                    guard let jsonTitle = json["title"] as? String else {
+                        print("could not get JSON title")
+                        return
+                    }
+                    print("The title is: " + jsonTitle)
+                } catch {
+                    print("error trying to convert data to JSON")
+                    return
+                }
+                }
+            task.resume()
+        }
+    }
 }
+
