707cc005d4727ad78227cae6a451513947948a8c
Mason Bourgeois <mason.bourgeois@gmail.com>
2018-04-11 15:14:39 -0500

    check for error on signup

--- ConnoisSearch/SignupViewController.swift
+++ ConnoisSearch/SignupViewController.swift
@@ -9,20 +9,39 @@
 import UIKit
 import Firebase
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-
+    @IBOutlet weak var signupBtn: UIButton!
+    
     @IBAction func signupBtnClick(_ sender: Any) {
       Auth.auth().createUser(withEmail: self.emailField.text!, password: self.passwordField.text!)
       { (user, error) in
-        self.performSegue(withIdentifier: "signedUpSuccess", sender: sender)
+        if let error = error as NSError? {
+          // Handle error
+          let alertController = UIAlertController (
+              title: "Signup Failed",
+              message: "Make sure you entered a valid email",
+              preferredStyle: UIAlertControllerStyle.alert)
+          let OKAction = UIAlertAction(
+              title: "OK",
+              style: UIAlertActionStyle.default) {
+                  (action:UIAlertAction) in
+                  print("OK button pressed");
+          }
+          alertController.addAction(OKAction)
+          self.present(alertController, animated: true, completion: nil)
+        } else {
+          self.performSegue(withIdentifier: "signedUpSuccess", sender: sender)
         }
+      }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        emailField.delegate = self
+        passwordField.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -31,6 +50,19 @@ class SignupViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String
+        ) -> Bool {
+        
+        if emailField.text!.count > 0 && passwordField.text!.count > 0 {
+            signupBtn.isEnabled = true
+        } else {
+            signupBtn.isEnabled = false
+        }
+        
+        return true
+    }
 
     /*
     // MARK: - Navigation
